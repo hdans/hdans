ad2ee27d4e3dd6d36223c6047c56d9984c7d7ac7
HDans <139644959+hdans@users.noreply.github.com>
2023-07-26 22:42:43 +0700

    Delete TugasAkhirKelasTerbuka directory

--- TugasAkhirKelasTerbuka/CRUD/__init__.py
+++
@@ -1,4 +0,0 @@
-# 9. Membentuk __init__ dan membuat import Database nya
-from . import Database
-from .Database import init_console
-from .view import read_console , create_console , update_console, delete_console
--- TugasAkhirKelasTerbuka/tes.py
+++
@@ -1,9 +0,0 @@
-import CRUD
-tes = """d s k a f u,23-07-26-10-26+0700,Aku si perasis handal                                                                                                                                                                                                                                          ,Fajar Racing                                                                                                                                                                                                                                                   ,2020"""
-print(len(tes))
-tes2 = """r l W J U e,23-07-26-10-08+0700,Cara tobat dari gay                                                                                                                                                                                                                                            ,Kibet Gay                                                                                                                                                                                                                                                      ,2030"""
-print(len(tes2))
-
-with open("DataTEST.txt" , "w" , encoding="utf-8") as file:
-    tesWoi = file.write("Agus bapak lo kahhh?")
-    file.write(tesWoi.rstrip("?"))
